52ec6bef2f6f0a6717ef06f23f6d4e1d9519861a
NkululekoMemela <nkululekolerato@gmail.com>
2024-01-07 16:46:52 +0200

    The ocean model validation function, it's execution script and test scripts are now ready for use. Keep them in the same directory.

--- execution_script.py
+++ execution_script.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # Define the input parameters
     dir_model = '/mnt/d/Run_False_Bay_2008_2018_SANHO/croco_avg_Y2013M*.nc.1'
     fname_obs = '/mnt/d/DATA-20231010T133411Z-003/DATA/ATAP/Processed/Data_Validation/FalseBaydata_FB001.nc'
-    fname_out = 'OutPut_Gambu_Jan_24.nc'
+    fname_out = 'OutPut_Msuthu!_Jan_24.nc'
 
     # Output file name and directory
     output_directory = "/mnt/d/Run_False_Bay_2008_2018_SANHO/Validation/ATAP/scripts/"
@@ -26,12 +26,10 @@ if __name__ == "__main__":
     ref_date = datetime(1990, 1, 1, 0, 0, 0)
     time_threshold = timedelta(hours=12)
     
-
-
     # Call the function
     get_model_obs_ts(dir_model, 
                      fname_obs, 
-                     fname_out, 
+                     fname_out, output_path,
                      var, 
                      ref_date=ref_date, 
                      depth=depth, 
